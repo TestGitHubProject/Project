674a2eb0fb784e91c9b2339c9c73b0563cca258c
nol0gic <nbg_yalta@hotmail.com>
2014-01-14 15:35:59 +0200

    *added controls, created player's prefab, scene updated

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Rotation : MonoBehaviour {
 
+	public float rotationSpeed;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey(KeyCode.E))
+		{
+			transform.RotateAround(transform.position, Vector3.up, rotationSpeed);
+		}
 	}
 }
